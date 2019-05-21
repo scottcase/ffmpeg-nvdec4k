2a9511c73c8fd2ea5134415fac7e3e2ae4bbaf6d
scottcase <scott.case.1@gmail.com>
2019-05-21 11:17:45 -0400

    initial commit

---
+++ buildffmpeg.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
+#git clone -b sdk/8.2 https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
+cd nv-codec-headers
+make
+make install
+cd ..
+
+wget http://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2 &> /dev/null
+tar xjvf ffmpeg-snapshot.tar.bz2 &> /dev/null
+cd ffmpeg
+
+./configure \
+--extra-cflags=-I/usr/local/cuda/targets/x86_64-linux/include \
+--extra-ldflags=-L/usr/local/cuda/targets/x86_64-linux/lib64 \
+--extra-cflags=-Ilocal/include \
+--enable-cuda \
+--enable-cuvid \
+--enable-nvenc \
+--enable-nonfree \
+--disable-shared \
+--enable-gpl \
+--enable-libass \
+--enable-libfdk-aac \
+--enable-libfreetype \
+--enable-libmp3lame \
+--enable-libopus \
+--enable-libtheora \
+--enable-libvorbis \
+--enable-libvpx \
+--enable-libxvid \
+--enable-version3 
+make -j 10 &> /dev/null
+make install
